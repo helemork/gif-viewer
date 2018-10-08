7dc444a925c757768e60f0745099edecca8adaaa
Helene Engeness Mørk <helemork@gmail.com>
2018-10-08 23:11:47 +0200

    Installing Qt and making very basic frame for app

--- main.cpp
+++ main.cpp
@@ -9,10 +9,53 @@ LEARNING THE BASICS
 
 #include <iostream>
 #include <string>
+#include <QWidget>
+#include <QApplication>
+#include <QIcon>
+#include <QLabel>
+#include <QHBoxLayout>
 
 using namespace std;
 
-int main() {
+// argc is how many input variables the command line needs
+// argv is the actual input parameters
+
+int main(int argc, char ** argv) {
+
+	// Initialise Qt.
+	QApplication app(argc, argv);
+
+	// MAKING WINDOW:
+	auto window = new QWidget();
+
+	// Customize the Window
+	window->setStyleSheet("background:#666666;padding:5px;");
+	window->setWindowTitle("The Amazing App");
+	window->setWindowIcon(QIcon(QString(ROOT_PATH)+"logo.png"));
+	auto topbar = new QHBoxLayout();
+	window->setLayout(topbar);
+
+	// Add picture inside the app
+	QImage logoimage(QString(ROOT_PATH) + "logo.png");
+	auto shrunk_logo = logoimage.scaledToHeight(40,Qt::SmoothTransformation);
+	auto logo = new QLabel();
+	logo->setPixmap(QPixmap::fromImage(shrunk_logo));
+	topbar->addWidget(logo);
+	
+	// Add page title
+	auto title = new QLabel();
+	title->setText("The Amazing App");
+	topbar->addWidget(title);
+
+
+	// SHOW WINDOW:
+	window->show();
+
+	// RUN PROGRAM
+	app.exec();
+
+
+	/*
 	cout << "THIS IS SOMETHING TO LOG. ";
 	cout << "speak, friend, and enter. ";
 
@@ -25,6 +68,7 @@ int main() {
 		cout << "Keep talking to close, you're accepted!";
 		cin >> result;
 	}
+	*/
 
 	return EXIT_SUCCESS;
 }
